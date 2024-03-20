90c5505cfcdee6e3982d861fbb7b693f43ffed40
Andrey Zhelezko <because.ahaha@gmail.com>
2024-03-20 07:46:31 +0300

    ff

--- src/js/main.js
+++ src/js/main.js
@@ -5,9 +5,39 @@ const msnry = new Masonry('.stories__items', {
     // columnWidth: 440
 });
 
-IMask(
-    document.querySelector('.js-mask-phone'),
-    {
-      mask: '+{7} (000) 000-00-00'
-    }
-)
+const accordion = () => {
+    // if (window.innerWidth > 767) return;
+    document.querySelectorAll('.js-accordion .accordion__header').forEach((el) => {
+        el.addEventListener('click', function() {
+
+            const self = el.closest('.accordion__item').classList.contains('is-active');
+            const $btn = el.querySelector('.button-arrow');
+
+            // document.querySelectorAll('.accordion__item').forEach((el) => {
+            //     // console.log(el)
+            //     const $btn = el.querySelector('.button-arrow');
+            //     el.classList.remove('is-active');
+            //     $btn.classList.toggle('button-arrow--down')
+            //     $btn.classList.toggle('button-arrow--up')
+            //     $btn.classList.toggle('button-arrow--filled')
+            // })
+            
+            // if (self) return;
+
+            $btn.classList.toggle('button-arrow--down')
+            $btn.classList.toggle('button-arrow--up')
+            $btn.classList.toggle('button-arrow--filled')
+
+            el.closest('.accordion__item').classList.toggle('is-active');
+        })
+    })
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    accordion();
+
+    IMask(document.querySelector('.js-mask-phone'), {
+          mask: '+{7} (000) 000-00-00'
+    })
+})
