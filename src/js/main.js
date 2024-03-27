293033b941583fab15cebe9b2b9d2c64eb9dffa0
Andrey Zhelezko <because.ahaha@gmail.com>
2024-03-28 02:53:24 +0300

    final

--- src/js/main.js
+++ src/js/main.js
@@ -101,9 +101,16 @@ const survey = () => {
     const $prev = $box.querySelector(".js-survey-prev");
     const $next = $box.querySelector(".js-survey-next");
 
-    function showResult() {
+    function showResult(step) {
+
+        const $form = $box.querySelector(
+            '.js-survey-step[data-step="' + step + '"]'
+        );
+
+        const result_popup_id = $form.querySelector('input[type="radio"]:checked').value;
+
         hidePopup("popup-test");
-        showPopup("popup-survey-result-1");
+        showPopup(result_popup_id);
     }
 
     function showStep(step) {
@@ -165,14 +172,21 @@ const survey = () => {
         $next.innerHTML = "Шаг " + (step + 1);
     });
     $next.addEventListener("click", function (e) {
-        step++;
-        if (!is_last) {
-            showStep(step);
-        } else {
-            showResult();
-        }
         $prev.style.display = "block";
         $next.disabled = true;
+        if (!is_last) {
+            step++;
+            showStep(step);
+        } else {
+            showResult(step);
+            $box.querySelectorAll('input[type="radio"]').forEach((radio) => {
+                radio.checked = false;
+            })
+            step = 1;
+            is_last= false;
+            $prev.style.display = "none";
+            showStep(step);
+        }
         $prev.innerHTML = "Шаг " + (step - 1);
         if (!is_last) {
             $next.innerHTML = "Шаг " + (step + 1);
@@ -211,6 +225,82 @@ const accordion = () => {
         });
 };
 
+const videos = () => {
+    document.querySelectorAll(".video").forEach((el) => {
+        el.addEventListener("click", function (e) {
+            e.preventDefault();
+            if (this.classList.contains("is-active") == false) {
+                this.classList.add("is-active");
+                this.querySelector("video").play();
+            }
+        });
+    });
+};
+
+const audioStop = () => {
+    document.querySelectorAll("audio").forEach((audio) => {
+        audio.pause();
+    });
+};
+
+const audio = () => {
+    const calculateTime = (secs) => {
+        const minutes = Math.floor(secs / 60);
+        const seconds = Math.floor(secs % 60);
+        const returnedSeconds =
+            seconds == 0 ? "00" : seconds < 10 ? `0${seconds}` : `${seconds}`;
+        const returnedMinutes =
+            minutes == 0 ? "00" : minutes < 10 ? `0${minutes}` : `${minutes}`;
+        return `${returnedMinutes}:${returnedSeconds}`;
+    };
+
+    document.querySelectorAll(".audio").forEach((el) => {
+        if (el.dataset.init == true) return;
+
+        const $audio = el.querySelector("audio");
+        if (!$audio) return;
+
+        const $progress = el.querySelector(".audio__duration");
+
+        $audio.addEventListener("loadedmetadata", () => {
+            $progress.textContent =
+                el.dataset.detailedDuration !== undefined
+                    ? "00:00 / " + calculateTime($audio.duration)
+                    : calculateTime($audio.duration);
+        });
+        $audio.addEventListener("timeupdate", () => {
+            $progress.textContent =
+                el.dataset.detailedDuration !== undefined
+                    ? calculateTime(Math.floor($audio.currentTime)) +
+                      " / " +
+                      calculateTime($audio.duration)
+                    : calculateTime(
+                          $audio.duration - Math.floor($audio.currentTime)
+                      );
+        });
+
+        el.querySelector(".audio__play .button-audio").addEventListener(
+            "click",
+            () => {
+                el.querySelector(".audio__play").classList.remove("is-active");
+                el.querySelector(".audio__pause").classList.add("is-active");
+                $audio.play();
+            }
+        );
+
+        el.querySelector(".audio__pause .button-audio").addEventListener(
+            "click",
+            () => {
+                el.querySelector(".audio__play").classList.add("is-active");
+                el.querySelector(".audio__pause").classList.remove("is-active");
+                $audio.pause();
+            }
+        );
+
+        el.dataset.init = true;
+    });
+};
+
 function hidePopup(id) {
     let popup = document.getElementById(id)
         ? document.getElementById(id)
@@ -232,21 +322,11 @@ function hidePopup(id) {
         );
         popup.classList.remove("is-shown");
         document.body.classList.remove("no-scroll");
+
+        if (id == "popup-teacher") {
+            audioStop();
+        }
     }
-}
-
-const videos = () => {
-
-    document.querySelectorAll('.video').forEach((el) => {
-        el.addEventListener('click', function(e) {
-            e.preventDefault();
-            if (this.classList.contains('is-active') == false) {
-                this.classList.add('is-active')
-                this.querySelector('video').play();
-            }
-        })
-    })
-
 }
 
 function showPopup(id, dataset) {
@@ -258,8 +338,8 @@ function showPopup(id, dataset) {
     if (id != "popup-lesson") hidePopup("popup-lesson");
     if (id != "popup-teacher") hidePopup("popup-teacher");
 
-    if (id == "popup-test") {
-        resetSurvey();
+    if (id == "popup-teacher") {
+        audio();
     }
 
     if (popup.dataset.processing && popup.dataset.processing == true) return;
@@ -274,6 +354,10 @@ function showPopup(id, dataset) {
                 if (this.status !== 200) return;
                 popup.querySelector(".popup-window").innerHTML =
                     this.responseText;
+
+                if (id == "popup-teacher") {
+                    audio();
+                }
             };
             xhr.send();
         }
@@ -287,11 +371,49 @@ function showPopup(id, dataset) {
     }
 }
 
+const tabsInit = () => {
+    const tabs = document.querySelectorAll(".js-tabs");
+
+    if (!tabs) return;
+
+    tabs.forEach((el) => {
+        const tabButtons = el.querySelectorAll(".js-tabs-button"),
+            contents = el.querySelectorAll(".js-tabs-content");
+
+        if (!tabButtons) return;
+        if (!contents) return;
+
+        tabButtons.forEach((tab) => {
+            tab.addEventListener("click", (e) => {
+                const id = e.target.dataset.id;
+
+                if (id) {
+                    tabButtons.forEach((btn) => {
+                        btn.classList.remove("is-active");
+                    });
+                    e.target.classList.add("is-active");
+
+                    contents.forEach((content) => {
+                        content.classList.remove("is-active");
+                    });
+
+                    if (!document.getElementById(id)) return;
+
+                    const element = document.getElementById(id);
+                    element.classList.add("is-active");
+                }
+            });
+        });
+    });
+};
+
 document.addEventListener("DOMContentLoaded", () => {
     accordion();
     survey();
     forms();
     videos();
+    audio();
+    tabsInit();
 
     IMask(document.querySelector(".js-mask-phone"), {
         mask: "+{7} (000) 000-00-00",
@@ -330,43 +452,53 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }
 
+    if (document.querySelector(".js-swiper-speak")) {
+        const $swiperSpeakPages = document.querySelector(
+            ".js-swiper-speak-pagination"
+        );
+        let progress = 0;
+        let realIndex;
+        let slidesqty = document
+            .querySelector(".js-swiper-speak")
+            .querySelectorAll(".swiper-slide").length;
+        for (let index = 0; index < slidesqty; index++) {
+            const div = document.createElement("div");
+            div.classList.add("speak__card__slider-page");
+            div.classList.add("js-swiper-speak-page");
+            $swiperSpeakPages.appendChild(div);
+        }
+        const swiperClients = new Swiper(".js-swiper-speak", {
+            autoplay: {
+                delay: 2500,
+                disableOnInteraction: false,
+            },
+            on: {
+                autoplayTimeLeft(s, time, progress) {
+                    progress = Math.round((1 - progress) * 100);
+                    $swiperSpeakPages.style.setProperty(
+                        "--progress",
+                        progress + "%"
+                    );
+                },
+                init: function (s) {},
+                slideChange: function (s) {
+                    if (s.realIndex != realIndex) {
+                        $swiperSpeakPages
+                            .querySelectorAll(".js-swiper-speak-page")
+                            .forEach((el) => {
+                                el.classList.remove("is-active");
+                            });
+                        $swiperSpeakPages
+                            .querySelectorAll(".js-swiper-speak-page")
+                            [s.realIndex].classList.add("is-active");
 
-    const $swiperSpeakPages = document.querySelector('.js-swiper-speak-pagination');
-    let progress = 0;
-    let realIndex;
-    let slidesqty = document.querySelector('.js-swiper-speak').querySelectorAll('.swiper-slide').length;
-    for (let index = 0; index < slidesqty; index++) {
-        const div = document.createElement("div");
-        div.classList.add("speak__card__slider-page");
-        div.classList.add("js-swiper-speak-page");
-        $swiperSpeakPages.appendChild(div);
+                        realIndex = s.realIndex;
+                    }
+                },
+            },
+            slidesPerView: "auto",
+            spaceBetween: 20,
+            loop: true,
+        });
     }
-    const swiperClients = new Swiper(".js-swiper-speak", {
-        autoplay: {
-            delay: 2500,
-            disableOnInteraction: false,
-        },
-        on: {
-            autoplayTimeLeft(s, time, progress) {
-                progress = Math.round((1 - progress)*100);
-                $swiperSpeakPages.style.setProperty("--progress", progress  + '%');
-            },
-            init: function(s) {
-            },
-            slideChange: function(s) {
-                if (s.realIndex != realIndex) {
-
-                    $swiperSpeakPages.querySelectorAll('.js-swiper-speak-page').forEach((el) => {
-                        el.classList.remove('is-active')
-                    });
-                    $swiperSpeakPages.querySelectorAll('.js-swiper-speak-page')[s.realIndex].classList.add('is-active');
-
-                    realIndex = s.realIndex;
-                }
-            }
-        },
-        slidesPerView: "auto",
-        spaceBetween: 20,
-        loop: true,
-    });
 });
